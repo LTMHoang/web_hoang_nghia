1a44556b3eb44c18868583940cddc2582133a714
LTMHoang <letruongminhhoang2003@gmail.com>
2022-08-21 22:54:50 +0700

    add js

--- assets/js/home.js
+++ assets/js/home.js
@@ -17,6 +17,21 @@ $(document).ready(function() {
         }, 1000);
     });
 
+//Làm hỗ trợ nhanh
+    $('#quickIcon').click(function() {
+        $('#quickIcon').hide('slow');
+
+        $('#quickMess').css('transform', 'translateY(0)');
+        $('#quickMess').css('transition', 'transform 0.7s linear');
+    });   
+    
+    $('#quickClose').click(function() {
+        $('#quickIcon').show('slow');
+
+        $('#quickMess').css('transform', 'translateY(100%)');
+        $('#quickMess').css('transition', 'transform 0.7s linear');
+    });
+
 //Để làm slider teacher mobile
 
     $('.teacher_next').click(function() {
