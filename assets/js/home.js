f2825fb066a145a68b7fe411c221095e4db2a2d5
LTMHoang <letruongminhhoang2003@gmail.com>
2022-08-26 11:27:43 +0700

    final

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,89 +1,91 @@
 $(document).ready(function() {
 
 //Để làm slider teacher mobile
+    let headT = $('.teacher_slider-inner a:first-child');
+    let tailT = $('.teacher_slider-inner a:last-child');
 
-    $('.teacher_next').click(function() {
-        let currentATeacher = $('.teacher_active');
-        let nextATeacher = currentATeacher.next();
+    $('.teacher_next').on('click', function () {
 
-        if(nextATeacher.length) {
-            currentATeacher.removeClass('teacher_active');
-            nextATeacher.addClass('teacher_active').css('animation', 'sliderLtR 1.5s 1');
-        }
+        let current = $('.teacher_active');
+        let next = current.next();
+
+        if(current.attr('rel') == tailT.attr('rel'))
+            next = headT;
+
+        current.removeClass('teacher_active');
+        next.addClass('teacher_active').css('animation', 'sliderLtR 1.5s 1');;
     });
 
-    $('.teacher_prev').click(function() {
-        let currentATeacher = $('.teacher_active');
-        let prevATeacher = currentATeacher.prev();
+    $('.teacher_prev').on('click', function () {
 
-        if(prevATeacher.length) {
-            currentATeacher.removeClass('teacher_active');
-            prevATeacher.addClass('teacher_active').css('animation', 'sliderRtL 1.5s 1');
-        }
+        let current = $('.teacher_active');
+        let prev = current.prev();
+
+        if(current.attr('rel') == headT.attr('rel'))
+            prev = tailT;
+
+        current.removeClass('teacher_active');
+        prev.addClass('teacher_active').css('animation', 'sliderRtL 1.5s 1');;
     });
+    
 
 //Để làm slider comment
+    let head = $('.slider-inner a:first-child');
+    let tail = $('.slider-inner a:last-child');
 
-    $('.next').click(function() {
-        let currentA = $('.active');
-        let nextA = currentA.next();
+    $('.next').on('click', function () {
 
-        if(nextA.length) {
-            currentA.removeClass('active');
-            nextA.addClass('active').css('animation', 'sliderLtR 1.5s 1');
-        }
+        let current = $('.active');
+        let next = current.next();
+
+        if(current.attr('rel') == tail.attr('rel'))
+            next = head;
+
+        current.removeClass('active');
+        next.addClass('active').css('animation', 'sliderLtR 1.5s 1');;
     });
 
-    $('.prev').click(function() {
-        let currentA = $('.active');
-        let prevA = currentA.prev();
+    $('.prev').on('click', function () {
 
-        if(prevA.length) {
-            currentA.removeClass('active');
-            prevA.addClass('active').css('animation', 'sliderRtL 1.5s 1');
-        }
+        let current = $('.active');
+        let prev = current.prev();
+
+        if(current.attr('rel') == head.attr('rel'))
+            prev = tail;
+
+        current.removeClass('active');
+        prev.addClass('active').css('animation', 'sliderRtL 1.5s 1');;
     });
+
+
+
 
 //Làm slider tự động
     setInterval(function () {
-        let head = $('.slider-inner a:first-child');
-        let tail = $('.slider-inner a:last-child');
 
         let current = $('.active');
         let next = current.next();
 
 
         if(current.attr('rel') == tail.attr('rel'))
-            {
-                current.removeClass('active');
-                next = head;
-                next.addClass('active').css('animation', 'sliderLtR 1.5s 1');
-            }
+            next = head;
 
         current.removeClass('active');
         next.addClass('active').css('animation', 'sliderLtR 1.5s 1');
 
-        
     }, 5000);   
     
     setInterval(function () {
-        let headT = $('.teacher_slider-inner a:first-child');
-        let tailT = $('.teacher_slider-inner a:last-child');
 
         let currentT = $('.teacher_active');
         let nextT = currentT.next();
 
 
         if(currentT.attr('rel') == tailT.attr('rel'))
-            {
-                currentT.removeClass('teacher_active');
-                nextT = headT;
-                nextT.addClass('teacher_active').css('animation', 'sliderLtR 1.5s 1');
-            }
+            nextT = headT;
 
         currentT.removeClass('teacher_active');
         nextT.addClass('teacher_active').css('animation', 'sliderLtR 1.5s 1');
 
-        
     }, 5000);   
 })
